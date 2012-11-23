0c37526dfa5837b163d227c6673a5c3b4897e019
constantin.boiangiu <constantin.boiangiu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-23 13:39:56 +0000

    Floating menu version 1.0.1 backup copy
    git-svn-id: https://plugins.svn.wordpress.org/codeflavors-floating-menu/tags/1.0.1@628999 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/cfm_menu.js
@@ -0,0 +1,58 @@
+/**
+ * @author: CodeFlavors [www.codeflavors.com]
+ * @version: 1.0.1
+ * @framework: jQuery
+ */
+
+(function($){
+	
+	$(document).ready(function(){
+		var menu = $('#cfn_floating_menu').find('ul').first(),
+			items = menu.children('li'),
+			options = $.parseJSON(CFM_MENU_PARAMS);
+		
+		$('#cfn_floating_menu').css({'top':options.top_distance});
+		
+		if( 1 == options.animate ){
+			$(window).scroll(function(e){
+				var st = $(window).scrollTop();
+				if( st > options.top_distance + 20 ){
+					$('#cfn_floating_menu').animate({'top':st+options.top_distance},{'queue':false, 'duration':500});
+				}else{
+					$('#cfn_floating_menu').animate({'top':options.top_distance},{'queue':false, 'duration':500});	
+				}
+			});		
+		}
+		
+		// show submenus
+		$(menu).find('li').mouseenter(function(){
+			$(this).children('ul').show(100);			
+		}).mouseleave(function(){
+			$(this).children('ul').hide(200);
+		});
+		
+		// highlight current item from menu
+		$(menu).find('li.current-menu-item').children('a').addClass('currentItem');
+		
+		// if first item is the trigger, show the menu only when hovering that item
+		if( $(items[0]).attr('id') == 'cfm_menu_title_li' ){			
+			var main = items.splice(0,1),
+				menuWidth = menu.width();
+			$(main).find('a').click(function(e){
+				e.preventDefault();
+			})
+			
+			$(items).hide();
+
+			$(menu).mouseenter(function(){
+				$(items).show(100);
+				$(main).animate({'width':menuWidth}, 100);
+			}).mouseleave(function(){
+				$(items).hide(200);
+				$(main).css('width', 'auto');
+			})			
+		}
+		
+	})	
+	
+})(jQuery);
